a1c9184b2f1ccb75975a00e490d0fd8b3d55cc2c
dousa <dsousa@adobe.com>
2020-02-03 11:28:02 -0800

    updates

--- src/databaseListeners/todoDatabaseListeners.js
+++ src/databaseListeners/todoDatabaseListeners.js
@@ -2,10 +2,9 @@ const os = require('os');
 const ip = require('ip');
 const version = require('../../package.json').version;
 const exec = require('child_process').exec;
-const promisify = require('promisify');
+const { promisify } = require('util');
 
 const timeStarted = (new Date()).toString();
-const execSync = promisify(exec);
 
 
 const todoAdded = async (socketMessageHandler, database) => {
