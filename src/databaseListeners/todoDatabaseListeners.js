11c54db927cfaa72420c8014aab06ff6a9323bff
dousa <dsousa@adobe.com>
2020-02-03 10:08:17 -0800

    test

--- src/databaseListeners/todoDatabaseListeners.js
+++ src/databaseListeners/todoDatabaseListeners.js
@@ -1,5 +1,5 @@
 const os = require('os');
-const ip = require('ip');
+// const ip = require('ip');
 
 const todoAdded = (socketMessageHandler, database) => {
 	return database.ref("/todos").on("child_added", snap => {
